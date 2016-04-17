588cc7f6324a3d2036388ee14f5c0caffbb7a30c
Loris Guignard <loris.guignard@gmail.com>
2016-04-17 20:32:56 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+  },
+  parser: 'babel-eslint',
+  extends: [
+    'airbnb/base',
+    'plugin:ava/recommended',
+    'plugin:lodash/recommended',
+  ],
+  plugins: [
+    'ava',
+    'lodash',
+  ],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+    },
+  },
+  rules: {
+    'lodash/prefer-lodash-method': 0,
+    'consistent-return': 0,
+    'no-console': 0,
+    'max-len': 0,
+    'no-param-reassign': [2, {
+      props: false,
+    }],
+    'id-length': [2, {
+      min: 2,
+      properties: 'never',
+      exceptions: ['_', 't'], // Allow lodash and ava
+    }],
+  },
+};
---
+++ react.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: [
+    './index.js',
+    'airbnb',
+  ],
+  plugins: ['react'],
+  rules: {},
+};
